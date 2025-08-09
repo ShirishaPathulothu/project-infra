633953a4ebd72faedec920c1ce3aa43f8184f326
Shirishapathulothu <pathulothushirisha@gmail.com>
2025-08-09 22:10:36 +0530

    terraform

---
+++ 50-app-alb/locals.tf
@@ -0,0 +1,7 @@
+locals {
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    resource_name = "${var.project_name}-${var.environment}"
+    private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
+
+
+}
