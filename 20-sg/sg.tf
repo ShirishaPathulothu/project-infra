a9f932271bcfbc80e67e9d34f837fbb3a02b6bad
Shirishapathulothu <pathulothushirisha@gmail.com>
2025-08-12 11:18:26 +0530

    terraform

--- 20-sg/sg.tf
+++ 20-sg/sg.tf
@@ -78,6 +78,7 @@ module "vpn_sg" {
     sg_tags = var.vpn_sg_tags
 }
 
+
 resource "aws_security_group_rule" "mysql_backend" {
   type              = "ingress"
   from_port         = 3306
@@ -256,4 +257,49 @@ resource "aws_security_group_rule" "backend_vpn_8080" {
   protocol          = "tcp"
   source_security_group_id = module.vpn_sg.id
   security_group_id = module.backend_sg.id
+}
+
+resource "aws_security_group_rule" "web_alb_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.web_alb_sg.id
+}
+
+resource "aws_security_group_rule" "web_alb_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.web_alb_sg.id
+}
+
+resource "aws_security_group_rule" "frontend_vpn" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = module.vpn_sg.id
+  security_group_id = module.frontend_sg.id
+}
+
+resource "aws_security_group_rule" "frontend_web_alb" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = module.web_alb_sg.id
+  security_group_id = module.frontend_sg.id
+}
+
+resource "aws_security_group_rule" "app_alb_frontend" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = module.frontend_sg.id
+  security_group_id = module.app_alb_sg.id
 }
